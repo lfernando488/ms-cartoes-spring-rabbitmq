d5c42d7e80270b1451e38f702273abc071dea900
Luiz Fernando <lfernando488@gmail.com>
2023-08-26 03:02:14 -0300

    Adicionando RabbitMQ ao serviço de cartoes

--- mscartoes/mscartoes/src/main/java/com/lfernando488/msspring/mscartoes/MscartoesApplication.java
+++ mscartoes/mscartoes/src/main/java/com/lfernando488/msspring/mscartoes/MscartoesApplication.java
@@ -1,10 +1,12 @@
 package com.lfernando488.msspring.mscartoes;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 //@EnableEurekaclient
+@EnableRabbit
 public class MscartoesApplication {
 
 	public static void main(String[] args) {
